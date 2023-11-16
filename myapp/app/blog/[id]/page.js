faa1c99189bc61ab56c7c7dabbc765e35182230e
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-11-16 16:41:39 -0500

    update

--- myapp/app/blog/[id]/page.js
+++ myapp/app/blog/[id]/page.js
@@ -1,26 +1,78 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react';
+
+// Dummy comments data for illustration
+const comments = [
+  { username: 'User1', comment: 'This is a great post!' },
+  { username: 'User2', comment: 'Nice content! Thanks for sharing.' },
+  { username: 'User3', comment: 'I have a question about this topic.' },
+];
 
 const BlogDetail = () => {
-  return (
-    <section className='relative h-screen'>
-       <div className='relative h-[600px]'>
-       <img
-        className='w-full h-full object-cover'
-        src='https://images.pexels.com/photos/3628508/pexels-photo-3628508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-       />
-       </div>
-       <div className='max-w-4xl h- mx-auto bg-white py-12 px-12 lg:px-24 -mt-32 relative z-10'>
-            <h2 className='mt-4 uppercase tracking-widest text-xs text-gray-600'>
-                11/16/2023
-            </h2>
-            <h1 className='font-display text-2xl md:text-3xl text-gray-900 mt-4'>Donut title</h1>
-            <div className='prose prose-sm sm:prose lg:prose-lg mt-6'>
-            <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
-            </div>
-       </div>
-    </section>
-  )
-}
+  const [newPost, setNewPost] = useState('');
+  const [posts, setPosts] = useState([]);
 
-export default BlogDetail
+  const handlePostSubmit = () => {
+    // Add the new post to the list of posts
+    setPosts([...posts, newPost]);
+    // Clear the new post input
+    setNewPost('');
+  };
+
+  return (
+    <section className='relative min-h-screen overflow-hidden'>
+      <div className='relative h-[600px]'>
+        <img
+          className='w-full h-full object-cover'
+          src='https://images.pexels.com/photos/3628508/pexels-photo-3628508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          alt='Blog Cover'
+        />
+      </div>
+      <div className='max-w-4xl mx-auto bg-white py-12 px-12 lg:px-24 -mt-32 relative z-10'>
+        <h2 className='mt-4 uppercase tracking-widest text-xs text-gray-600'>
+          11/16/2023
+        </h2>
+        <h1 className='font-display text-2xl md:text-3xl text-gray-900 mt-4'>
+          Donut title
+        </h1>
+        <div className='prose prose-sm sm:prose lg:prose-lg mt-6'>
+          <p>
+            Leverage agile frameworks to provide a robust synopsis for high
+            level overviews. Iterative approaches to corporate strategy foster
+            collaborative thinking to further the overall value proposition.
+            Organically grow the holistic world view of disruptive innovation
+            via workplace diversity and empowerment.
+          </p>
+        </div>
+
+        <div className='mt-12'>
+          <h2 className='text-xl font-bold text-gray-900 mb-4'>Comments</h2>
+          {comments.map((comment, index) => (
+            <div key={index} className='bg-gray-100 p-4 mb-4'>
+              <p className='text-gray-800 font-bold'>{comment.username}</p>
+              <p className='text-gray-800'>{comment.comment}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className='mt-8'>
+          <h2 className='text-xl font-bold text-gray-900 mb-4'>Add New Post</h2>
+          <textarea
+            className='w-full h-32 p-2 border border-gray-300 rounded mb-4'
+            placeholder='Type your new post here...'
+            value={newPost}
+            onChange={(e) => setNewPost(e.target.value)}
+          />
+          <button
+            className='bg-purple-500 text-white px-4 py-2 rounded w-full'
+            onClick={handlePostSubmit}
+          >
+            Post
+          </button>
+        </div>
+   </div>
+    </section>
+  );
+};
+
+export default BlogDetail;
--- myapp/components/Navbar/Navbar.js
+++ myapp/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
+
 'use client'
 import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { FiMenu, FiMail } from 'react-icons/fi'; // Import the necessary icons
-import { Transition } from '@headlessui/react';
 import Link from 'next/link';
 
 const Navbar = () => {
@@ -33,25 +34,31 @@ const Navbar = () => {
       </div>
 
       {/* Full-screen menu */}
-      <Transition
-        show={isMenuOpen}
-        enter="transition-opacity ease-out duration-300"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="transition-opacity ease-in duration-300"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        <div className="fixed inset-0 bg-black bg-opacity-75 z-50">
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <div className="bg-white text-gray-900 p-8 space-y-4">
-              <Link href="/">Home</Link>
-              <Link href="/">Home</Link>
-              <Link href="/">Home</Link>
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="fixed inset-0 bg-black bg-opacity-75 z-50"
+          >
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <div className="bg-white text-gray-500 p-8 space-y-4 w-[400px] text-center">
+              <div>
+              <Link className='mr-6' href="/">Home</Link>
+                <Link className='mr-6'  href="/blog">Blog</Link>
+                <Link className='mr-6'  href="/about">About</Link>
+                <Link className='mr-6'  href="/contact">Contact</Link>
+              </div>
+              <div>
+                Auth
+              </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </Transition>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   );
 };
